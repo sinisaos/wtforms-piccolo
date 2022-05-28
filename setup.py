6cfb267a85c1e9861c4243afc763ddf903fceaa2
sinisaos <sinisaos@gmail.com>
2022-05-28 06:06:29 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wtforms_piccolo",
-    version="0.1.2",
+    version="0.2.0",
     author="sinisaos",
     author_email="sinisaos@gmail.com",
     description="Form generation utilities for Piccolo ORM Table class",
